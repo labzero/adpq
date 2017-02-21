5a1f3a31164bebc219f8d6585156c0cb262a3fc8
svoynow <sasha@labzero.com>
2017-02-21 13:18:02 -0800

    Update catalog_item tests for new schema (#34)

--- test/models/catalog_item_test.exs
+++ test/models/catalog_item_test.exs
@@ -3,7 +3,23 @@ defmodule Adpq.CatalogItemTest do
 
   alias Adpq.CatalogItem
 
-  @valid_attrs %{category: "some content", clin: "some content", contract_discount: 42, contract_unit_price: 42, description: "some content", list_price: 42, manufacturer: "some content", quantity_in_uom: 42, sku: "some content", super_category: "some content", unit_of_measure: "some content", unspc: "some content"}
+  @valid_attrs %{
+    long_category: "long",
+    clin: "abcd",
+    name: "A Computer",
+    contract_discount: 42,
+    contract_unit_price: 42,
+    description: "lorem ipsum",
+    list_price: 42,
+    manufacturer: "HP",
+    quantity_in_uom: 42,
+    sku: "A-123",
+    simple_category: "simple",
+    top_level_category: "top level",
+    unit_of_measure: "some content",
+    unspc: "some content"
+  }
+
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
