ac113ba771e83090371b7e1d78de45c591482b07
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-02-22 13:50:59 -0800

    Remove non-functional links

--- web/static/js/components/Footer/Footer.js
+++ web/static/js/components/Footer/Footer.js
@@ -6,29 +6,23 @@ const Footer = () => (
       <div className="usa-grid-full">
         <div className="usa-footer-nav">
           <ul className="usa-unstyled-list">
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Laptops">Laptops</a>
             </li>
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Desktops">Desktops</a>
             </li>
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Peripherals">Peripherals</a>
             </li>
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Components">Components</a>
             </li>
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Software">Software</a>
             </li>
-            <li className="usa-width-one-fourth">
+            <li className="usa-width-one-third">
               <a className="usa-footer-primary-link" href="/category/Services">Services</a>
-            </li>
-            <li className="usa-width-one-fourth">
-              <a className="usa-footer-primary-link" href="/category/Brands">Brands</a>
-            </li>
-            <li className="usa-width-one-fourth">
-              <a className="usa-footer-primary-link" href="/category/Publishers">Publishers</a>
             </li>
           </ul>
         </div>
