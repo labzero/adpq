a80a189bbc5f4f5a20c8c83afdbf0c5d3362f59f
Adam Ducker <aducker@apple.com>
2017-02-25 10:58:39 -0800

    Add styles and logic to the login view to hide nav elements

--- web/static/js/components/LoginForm/LoginForm.js
+++ web/static/js/components/LoginForm/LoginForm.js
@@ -2,25 +2,39 @@ import React, { PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class LoginForm extends React.Component {
-
   static propTypes = {
     handleSubmit: PropTypes.func.isRequired
   }
 
+  state = {
+    passwordInput: 'password'
+  }
+
+  togglePassword = _event => (
+      this.setState(prevState => ({ passwordInput: (prevState.passwordInput === 'password' ? 'text' : 'password') }))
+  )
+
   render() {
     const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Username</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <Field name="password" component="input" type="password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="usa-section">
+        <h2>Sign in</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Username or email address</label>
+            <Field name="name" component="input" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <Field name="password" component="input" type={this.state.passwordInput} />
+            <div className="login-show-password"><a href="#show-password" onClick={this.togglePassword}>Show password</a></div>
+          </div>
+          <button type="submit">Sign in</button>
+
+          <p><a href="#">Forgot username?</a></p>
+          <p><a href="#">Forgot password?</a></p>
+        </form>
+      </div>
     );
   }
 }
