08b9b491143f40737284fec89817566fc5e46192
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-02-25 03:23:33 -0800

    Add category page (#97)
    * Add working filters
    
    * Add Category and query tests
    
    * Another test for two different filters
    
    * Fix specs
    
    * Fix spec typo
    
    * Fix changes broken during merge

--- web/static/js/store/index.js
+++ web/static/js/store/index.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
+import { browserHistory } from 'react-router'
+import { routerMiddleware } from "react-router-redux";
 
 import reducer from "../reducers/index";
 
@@ -8,6 +10,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore() {
   return createStore(
     reducer,
-    composeEnhancers(applyMiddleware(thunkMiddleware))
+    composeEnhancers(applyMiddleware(thunkMiddleware), applyMiddleware(routerMiddleware(browserHistory)))
   );
 }
