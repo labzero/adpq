2254aefe16bb5ab19289c1d7b273dcb055771500
Adam Ducker <aducker@apple.com>
2017-02-25 10:58:39 -0800

    Add styles and logic to the login view to hide nav elements

--- web/static/js/components/Footer/Footer.js
+++ web/static/js/components/Footer/Footer.js
@@ -1,101 +1,103 @@
 import React, { PropTypes } from 'react';
 
-const Footer = ({ isHomepage }) => {
+const Footer = ({ isAuthorized, isHomepage }) => {
   let nav;
-  if (isHomepage) {
+  if (isHomepage && isAuthorized) {
     nav = (
-      <div className="usa-grid-full footer-grid">
-        <nav className="usa-footer-nav">
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Laptops">Laptops</a>
-            </li>
-            <li><a href="/category/Laptops?filter=simple_category:Ultralight">Ultralight</a></li>
-            <li><a href="/category/Laptops?filter=simple_category:Standard">Standard</a></li>
-            <li><a href="/category/Laptops?filter=simple_category:Performance">Performance</a></li>
-            <li><a href="/category/Laptops?filter=simple_category:Workstation">Workstation</a></li>
-          </ul>
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Desktops">Desktops</a>
-            </li>
-            <li><a href="/category/Desktops?filter=simple_category:Thin Client">Thin Client</a></li>
-            <li><a href="/category/Desktops?filter=simple_category:Standard">Standard</a></li>
-            <li><a href="/category/Desktops?filter=simple_category:Performance">Performance</a></li>
-            <li><a href="/category/Desktops?filter=simple_category:Workstation">Workstation</a></li>
-            <li><a href="/category/Desktops?filter=simple_category:All-in-One">All-in-One</a></li>
-          </ul>
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Peripherals">Peripherals</a>
-            </li>
-            <li><a href="/category/Peripherals?filter=simple_category:Displays">Displays</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Headsets Microphones %26 Speakers">Headsets, Microphones, &amp; Speakers</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Keyboards %26 Mice">Keyboards &amp; Mice</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Cables %26 Adapters">Cables &amp; Adapters</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Cases %26 Bags">Cases &amp; Bags</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Docking Stations">Docking Stations</a></li>
-            <li><a href="/category/Peripherals?filter=simple_category:Mounting">Mounting</a></li>
-          </ul>
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Components">Components</a>
-            </li>
-            <li><a href="/category/Components?filter=simple_category:Processors">Processors</a></li>
-            <li><a href="/category/Components?filter=simple_category:Memory">Memory</a></li>
-            <li><a href="/category/Components?filter=simple_category:Video Cards">Video Cards</a></li>
-            <li><a href="/category/Components?filter=simple_category:Storage Devices">Storage Devices</a></li>
-          </ul>
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Software">Software</a>
-            </li>
-            <li><a href="/category/Software?filter=simple_category:Design">Design</a></li>
-            <li><a href="/category/Software?filter=simple_category:Productivity">Productivity</a></li>
-          </ul>
-          <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
-            <li>
-              <a className="footer-primary-link usa-footer-primary-link" href="/category/Services">Services</a>
-            </li>
-            <li><a href="/category/Services?filter=simple_category:Support">Support</a></li>
-          </ul>
-        </nav>
+      <div className="footer-primary usa-footer-primary-section">
+        <div className="usa-grid-full footer-grid">
+          <nav className="usa-footer-nav">
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Laptops">Laptops</a>
+              </li>
+              <li><a href="/category/Laptops?filter=simple_category:Ultralight">Ultralight</a></li>
+              <li><a href="/category/Laptops?filter=simple_category:Standard">Standard</a></li>
+              <li><a href="/category/Laptops?filter=simple_category:Performance">Performance</a></li>
+              <li><a href="/category/Laptops?filter=simple_category:Workstation">Workstation</a></li>
+            </ul>
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Desktops">Desktops</a>
+              </li>
+              <li><a href="/category/Desktops?filter=simple_category:Thin Client">Thin Client</a></li>
+              <li><a href="/category/Desktops?filter=simple_category:Standard">Standard</a></li>
+              <li><a href="/category/Desktops?filter=simple_category:Performance">Performance</a></li>
+              <li><a href="/category/Desktops?filter=simple_category:Workstation">Workstation</a></li>
+              <li><a href="/category/Desktops?filter=simple_category:All-in-One">All-in-One</a></li>
+            </ul>
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Peripherals">Peripherals</a>
+              </li>
+              <li><a href="/category/Peripherals?filter=simple_category:Displays">Displays</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Headsets Microphones %26 Speakers">Headsets, Microphones, &amp; Speakers</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Keyboards %26 Mice">Keyboards &amp; Mice</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Cables %26 Adapters">Cables &amp; Adapters</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Cases %26 Bags">Cases &amp; Bags</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Docking Stations">Docking Stations</a></li>
+              <li><a href="/category/Peripherals?filter=simple_category:Mounting">Mounting</a></li>
+            </ul>
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Components">Components</a>
+              </li>
+              <li><a href="/category/Components?filter=simple_category:Processors">Processors</a></li>
+              <li><a href="/category/Components?filter=simple_category:Memory">Memory</a></li>
+              <li><a href="/category/Components?filter=simple_category:Video Cards">Video Cards</a></li>
+              <li><a href="/category/Components?filter=simple_category:Storage Devices">Storage Devices</a></li>
+            </ul>
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Software">Software</a>
+              </li>
+              <li><a href="/category/Software?filter=simple_category:Design">Design</a></li>
+              <li><a href="/category/Software?filter=simple_category:Productivity">Productivity</a></li>
+            </ul>
+            <ul className="usa-unstyled-list usa-width-one-third usa-footer-primary-content">
+              <li>
+                <a className="footer-primary-link usa-footer-primary-link" href="/category/Services">Services</a>
+              </li>
+              <li><a href="/category/Services?filter=simple_category:Support">Support</a></li>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
-  } else {
+  } else if (isAuthorized) {
     nav = (
-      <div className="usa-grid footer-grid">
-        <nav className="usa-footer-nav">
-          <ul className="usa-unstyled-list">
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Laptops">Laptops</a>
-            </li>
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Desktops">Desktops</a>
-            </li>
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Peripherals">Peripherals</a>
-            </li>
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Components">Components</a>
-            </li>
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Software">Software</a>
-            </li>
-            <li className="usa-width-one-third">
-              <a className="usa-footer-primary-link" href="/category/Services">Services</a>
-            </li>
-          </ul>
-        </nav>
+      <div className="footer-primary usa-footer-primary-section">
+        <div className="usa-grid footer-grid">
+          <nav className="usa-footer-nav">
+            <ul className="usa-unstyled-list">
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Laptops">Laptops</a>
+              </li>
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Desktops">Desktops</a>
+              </li>
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Peripherals">Peripherals</a>
+              </li>
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Components">Components</a>
+              </li>
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Software">Software</a>
+              </li>
+              <li className="usa-width-one-third">
+                <a className="usa-footer-primary-link" href="/category/Services">Services</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
   }
 
   return (
     <footer className={`footer usa-footer ${isHomepage ? 'usa-footer-big' : 'footer-slim usa-footer-slim'}`}>
-      <div className="footer-primary usa-footer-primary-section">
-        {nav}
-      </div>
+      {nav}
       <div className="footer-secondary usa-footer-secondary_section">
         <div className="usa-grid">
           <div className="footer-logo usa-footer-logo usa-width-two-thirds">
@@ -112,7 +114,8 @@ const Footer = ({ isHomepage }) => {
 };
 
 Footer.propTypes = {
-  isHomepage: PropTypes.bool.isRequired
+  isHomepage: PropTypes.bool.isRequired,
+  isAuthorized: PropTypes.bool.isRequired
 };
 
 export default Footer;
--- web/static/js/components/Header/Header.js
+++ web/static/js/components/Header/Header.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router';
 class Header extends Component {
 
   static propTypes = {
-    section: PropTypes.string.isRequired
+    section: PropTypes.string.isRequired,
+    isAuthorized: PropTypes.bool.isRequired
   }
 
   state = {
@@ -20,21 +21,10 @@ class Header extends Component {
   render() {
     const { openSubmenu } = this.state;
 
-    return (
-      <header className="header usa-header usa-header-extended" role="banner">
-        <div className="usa-navbar">
-          <button className="usa-menu-btn">Menu</button>
-          <div className="logo-container">
-            <div className="logo-department">
-                California Department of Technology
-              </div>
-            <div className="logo usa-logo" id="logo">
-              <em className="logo-text usa-logo-text">
-                <a href="/" accessKey="1" title="Home" aria-label="Home">Tech Shop</a>
-              </em>
-            </div>
-          </div>
-        </div>
+    let nav;
+
+    if (this.props.isAuthorized) {
+      nav = (
         <nav role="navigation" className="nav usa-nav">
           <div className="usa-nav-inner">
             <button className="usa-nav-close">
@@ -118,13 +108,32 @@ class Header extends Component {
                 </li>
                 <li>
                   <Link to={'logout'}>
-                      Logout
-                    </Link>
+                    Logout
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
+      );
+    }
+
+    return (
+      <header className="header usa-header usa-header-extended" role="banner">
+        <div className="usa-navbar">
+          <button className="usa-menu-btn">Menu</button>
+          <div className="logo-container">
+            <div className="logo-department">
+              California Department of Technology
+            </div>
+            <div className="logo usa-logo" id="logo">
+              <em className="logo-text usa-logo-text">
+                <a href="/" accessKey="1" title="Home" aria-label="Home">Tech Shop</a>
+              </em>
+            </div>
+          </div>
+        </div>
+        {nav}
       </header>
     );
   }
