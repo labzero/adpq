ffedf641adaf0479f303c4a4a6896f75c994583e
Chris Bucchere <chris@bucchere.com>
2017-03-01 12:36:17 -0800

    add currency formatting for amount

--- web/static/js/components/OrderTable/OrderTable.js
+++ web/static/js/components/OrderTable/OrderTable.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import currencyFormatter from 'currency-formatter';
 
 class OrderTable extends Component {
   static propTypes = {
@@ -33,7 +34,7 @@ class OrderTable extends Component {
                 <td>{order.inserted_at}</td>
                 <td>{order.user_id}</td>
                 <td>{order.status}</td>
-                <td>{order.items.reduce((acc, order_item) => acc + (order_item.price * order_item.quantity), 0)}</td>
+                <td>{currencyFormatter.format(order.items.reduce((acc, order_item) => acc + (order_item.price * order_item.quantity), 0) / 100, { code: 'USD' })}</td>
               </tr>
             ))}
           </tbody>
