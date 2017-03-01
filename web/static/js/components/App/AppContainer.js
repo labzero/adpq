0a36a29fa59b31c9cace1c2effa400b9599990db
Adam Ducker <adam@ozarkwebdevelopers.com>
2017-02-28 18:43:09 -0800

    Update the footer and header for admin view (#124)
    * Update the footer and header for admin view
    
    * Fix header menu highlighting

--- web/static/js/components/App/AppContainer.js
+++ web/static/js/components/App/AppContainer.js
@@ -20,16 +20,40 @@ const findSection = category => (
   Object.keys(sections).find(section => sections[section].indexOf(category) !== -1)
 );
 
+const findHeaderMode = (location) => {
+  let mode = 'default';
+  if (location.pathname === '/login') {
+    mode = 'login';
+  } else if (location.pathname === '/admin') {
+    mode = 'admin';
+  }
+  return mode;
+};
+
+const findFooterMode = (location) => {
+  let mode = 'less';
+  if (location.pathname === '/login' || location.pathname === '/admin') {
+    mode = 'login';
+  } else if (location.pathname === '/') {
+    mode = 'more';
+  }
+  return mode;
+};
+
 const mapStateToProps = (state, ownProps) => {
+  const location = ownProps.location;
   let section;
   if (ownProps.params.id) {
-    const item = state.catalog.items.find(it => it.id === ownProps.params.id);
+    // eslint-disable-next-line eqeqeq
+    const item = state.catalog.items.find(it => it.id == ownProps.params.id);
     section = item && findSection(item.top_level_category);
   } else if (ownProps.params.name) {
     section = findSection(ownProps.params.name);
   }
+  const headerMode = findHeaderMode(location);
+  const footerMode = findFooterMode(location);
 
-  return { section, ...ownProps };
+  return { section, headerMode, footerMode, ...ownProps };
 };
 
 export default connect(
