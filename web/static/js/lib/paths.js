8003430c72718228eb58e0a97672d40597bf9482
svoynow <sasha@labzero.com>
2017-03-02 03:27:03 -0800

    URI encode SKUs to prevent breaking on # (#169)

--- web/static/js/lib/paths.js
+++ web/static/js/lib/paths.js
@@ -1,2 +1,2 @@
-export const catalogItemPath = item => `/item/${item.manufacturer}-${item.sku}`;
+export const catalogItemPath = item => `/item/${item.manufacturer}-${encodeURIComponent(item.sku)}`;
 export { catalogItemPath as default };
