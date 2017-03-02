2d80476d6184c92c95b3bdaa23d861bb49eb48de
Sasha Voynow <sasha@labzero.com>
2017-03-02 09:59:30 -0800

    Add username and department to order detail

--- web/views/order_view.ex
+++ web/views/order_view.ex
@@ -12,6 +12,8 @@ defmodule Adpq.OrderView do
   def render("order.json", %{order: order}) do
     %{id: order.id,
       user_id: order.user_id,
+      username: order.user.name,
+      department: order.user.department,
       status: order.status,
       inserted_at: Adpq.ViewHelpers.unix_timestamp(order.inserted_at),
       items: render_many(order.order_items, Adpq.OrderItemView, "order_item.json")
