35b5d5e7185f9023612efb0a6d9bc52c9190aa3b
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-02 12:10:03 -0800

    Add admin catalog styles (#182)

--- web/static/js/components/Thanks/Thanks.js
+++ web/static/js/components/Thanks/Thanks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AlertsContainer from '../Alerts/AlertsContainer';
 
 const Thanks = () => (
-  <div className="usa-grid">
+  <div className="thanks usa-grid">
     <div className="usa-section">
       <h2>Thanks</h2>
     </div>
