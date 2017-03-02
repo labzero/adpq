5fa395fb16f01afc6e7ad449093cff2540cdd32f
Sasha Voynow <sasha@labzero.com>
2017-03-01 20:46:03 -0800

    Add category specific product images

--- test/javascript/lib/image_urls.spec.js
+++ test/javascript/lib/image_urls.spec.js
@@ -4,10 +4,14 @@ test("it returns a specific item match", () => {
   expect(catalogItemImage({manufacturer: 'HP', sku: 'F5A53AA#ABA'})).toEqual('/images/products/thin-client-hp.jpg')
 })
 
+test("it returns a category match", () => {
+  expect(catalogItemImage({manufacturer: 'HP', top_level_category: 'Components', simple_category: 'Video Cards'})).toEqual('/images/products/Components-Video%20Cards.jpg')
+})
+
 test("it returns a manufacturer top level category match", () => {
-  expect(catalogItemImage({manufacturer: 'HP', top_level_category: 'Laptops'})).toEqual('/images/products/everyday-computing-hp-laptop.jpg')
+  expect(catalogItemImage({manufacturer: 'HP', top_level_category: 'Laptops', })).toEqual('/images/products/everyday-computing-hp-laptop.jpg')
 })
 
 test("it returns a default image when no match is found", () => {
-  expect(catalogItemImage({})).toEqual('/images/products/everyday-computing-dell-desktop.jpg')
+  expect(catalogItemImage({})).toEqual('/images/products/Components-Storage%20Devices.jpg')
 })
