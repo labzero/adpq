591e924f3b34fa26a7ef3615f794728761af3f35
Adam Ducker <aducker@apple.com>
2017-03-02 16:49:40 -0800

    Total item quantities for header cart count

--- web/static/js/components/Header/HeaderDefaultContainer.js
+++ web/static/js/components/Header/HeaderDefaultContainer.js
@@ -3,7 +3,7 @@ import HeaderDefault from './HeaderDefault';
 
 const mapStateToProps = (state, ownProps) => ({
   section: ownProps.section,
-  cartCount: state.cart.items.length
+  cartCount: state.cart.items.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
 });
 
 export default connect(mapStateToProps)(HeaderDefault);
